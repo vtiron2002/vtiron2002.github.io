e0d72e9661b288641f1b97a8d4eb6cb5084acbde
Valentyn Tiron <vt02@Valentyns-MacBook-Pro.local>
2020-07-01 16:27:09 -0400

    first commit

---
+++ precache-manifest.4a6a226e30ffdaaacf0a7af985f172c3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f67ddd0ade94742b739013cc8b5d3b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ca1a894c191c4d4eebc8",
+    "url": "/portfolio/static/css/main.2299ad9f.chunk.css"
+  },
+  {
+    "revision": "7b1863067fcccbc57e72",
+    "url": "/portfolio/static/js/2.34d62d34.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/portfolio/static/js/2.34d62d34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca1a894c191c4d4eebc8",
+    "url": "/portfolio/static/js/main.d66e8c19.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "1d1fbd99cb7b75b67d09bf5721b9e2be",
+    "url": "/portfolio/static/media/bookRooms.1d1fbd99.png"
+  },
+  {
+    "revision": "59bf3b03db55bc436d38a909d7bec116",
+    "url": "/portfolio/static/media/dark-help-page.59bf3b03.png"
+  },
+  {
+    "revision": "08332c4bc82abb4f222c0d75d3befa71",
+    "url": "/portfolio/static/media/fastTechSite.08332c4b.png"
+  },
+  {
+    "revision": "a0e2354c7f7a8716dbbf0545c1d4a0ab",
+    "url": "/portfolio/static/media/loginPage.a0e2354c.png"
+  },
+  {
+    "revision": "434e548fb56700e273af1da5bbae4ca5",
+    "url": "/portfolio/static/media/mernap.434e548f.png"
+  },
+  {
+    "revision": "8984cd76fbcb7cbf8b1bfaf38cf13241",
+    "url": "/portfolio/static/media/mobileUI.8984cd76.png"
+  },
+  {
+    "revision": "f7b17dac3d10588f4dc352a238009ef3",
+    "url": "/portfolio/static/media/noteman.f7b17dac.png"
+  }
+]);
